e5c5001b496f8e7e17445c5853d14f3b72444b61
Billy Gisbourne <billy.gisbourne@student.anglia.ac.uk>
2017-01-18 23:27:16 +0000

    Fixed wrong variable name

--- AlqoholicTwitch/AlqoholicTwitch.lua
+++ AlqoholicTwitch/AlqoholicTwitch.lua
@@ -199,7 +199,7 @@ function AlqoholicTwitch:KillableWithE(range)
 	local canKill = false
 	for i = 1, Game.HeroCount() do
 		local hero = Game.Hero(i)
-		if self:IsValidTarget(hero, range) and hero.team ~= myHero.team and self:HasBuff(hero, _passiveBuffName) and (getdmg(_E, eTarget, myHero) > eTarget.health) then
+		if self:IsValidTarget(hero, range) and hero.team ~= myHero.team and self:HasBuff(hero, _passiveBuffName) and (getdmg(_E, hero, myHero) > hero.health) then
 			canKill = true
 			break
 		end
